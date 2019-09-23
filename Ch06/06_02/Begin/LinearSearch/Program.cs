964fae0701b3aac8eee498fe6472416a6ca0f319
Bhaumik Dhandhukia <bhaumik@rpmsoftware.com>
2019-09-23 12:18:48 -0600

    added linear code

--- Ch06/06_02/Begin/LinearSearch/Program.cs
+++ Ch06/06_02/Begin/LinearSearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace LinearSearch
 {
@@ -7,6 +8,28 @@ namespace LinearSearch
         static void Main(string[] args)
         {
             //TODO
+            int theValue = 7;
+            int[] array = new int[] { 1, 4, 5, 7, 9, 22 };
+
+            WriteLine("Our arr contains: ");
+            Array.ForEach(array, x => Write(x + " "));
+            Write($"\n\n The result of the linear search for the {theValue} is : ");
+            WriteLine(linearSearch(array, theValue));
+        }
+
+        static int linearSearch(int[] a, int x)
+        {
+            //int answer = -1;
+
+            for(int i = 0; i< a.Length; i++)
+            {
+                if(a[i] == x)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
